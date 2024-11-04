2fecb2af6201f93a2cdce637aaddc19fee605eac
c4dd3 <danielowu1012@gmail.com>
2024-11-04 00:07:11 -0600

    UserConfig_Finish_04/11/2024-00.07

--- rise-fund-android-version/controllers/USER_Config_Controller.js
+++ rise-fund-android-version/controllers/USER_Config_Controller.js
@@ -12,7 +12,6 @@ export const getUserInfo = async (givenID) => {
       // Devuelve los datos del usuario obtenidos
       return result[0][0];
     } else {
-      console.log('Usuario no encontrado');
       Alert.alert(
         'User Not Found',
         'No user found with the provided ID.',
@@ -21,13 +20,111 @@ export const getUserInfo = async (givenID) => {
       return null;
     }
   } catch (error) {
-    console.error('Error obteniendo información del usuario:', error);
+    console.error('Error retrieving user information:', error);
     Alert.alert(
       'Error',
       'There was an error retrieving user information. Please try again later.',
       [{ text: 'OK' }]
     );
     return null;
+  }
+};
+
+export const getAllRoles = async () => {
+  try {
+    const procedureName = 'sp_get_all_roles';
+    const params = {}; 
+    const result = await executeProcedure(procedureName, params);
+    
+    return result[0]; // Devuelve los datos de todos los roles
+  } catch (error) {
+    console.error('Error fetching roles:', error);
+    throw error;
+  }
+};
+
+export const getUserAccountInfo = async (userId) => {
+  try {
+    const procedureName = 'sp_get_payment_account_by_user_id'; // Asegúrate de que este SP esté en la base de datos
+    const params = { UserId: userId };
+    const result = await executeProcedure(procedureName, params);
+    return result[0][0]; // Devuelve la primera fila con la información de la cuenta
+  } catch (error) {
+    console.error('Error fetching user account info:', error);
+    throw error;
+  }
+};
+
+
+
+
+ const getAccountInfoByAccountNumber = async (accountNumber) => {
+  try {
+    const procedureName = 'sp_get_account_info_by_account_number'; // Asegúrate de que este SP esté en la base de datos
+    const params = { AccountNumber: accountNumber };
+    const result = await executeProcedure(procedureName, params);
+    return result[0]; // Devuelve la primera fila con la información de la cuenta
+  } catch (error) {
+    console.error('Error fetching account info by account number:', error);
+    throw error;
+  }
+};
+
+export const generateUniqueAccountNumber = async () => {
+  let isUnique = false;
+  let accountNumber;
+  
+  while (!isUnique) {
+    // Genera un número de cuenta aleatorio de 16 dígitos
+    accountNumber = Math.floor(1000000000000000 + Math.random() * 9000000000000000).toString();
+    // Verifica si el número de cuenta ya existe
+    const existingAccount = await getAccountInfoByAccountNumber(accountNumber);
+    if (existingAccount.length == 0) {
+      isUnique = true;
+    }
+  }
+
+  return accountNumber;
+};
+
+export const insertPaymentAccount = async (accountNumber, userId, cardNumber, expirationDate, securityNumber, balance) => {
+  try {
+    const procedureName = 'sp_insert_payment_account';
+    const params = {
+      AccountNumber: accountNumber,
+      UserId: userId,
+      CardNumber: cardNumber,
+      ExpirationDate: expirationDate,
+      SecurityNumber: securityNumber,
+      Balance: balance,
+      Status: 1 // Activo por defecto
+    };
+
+    const result = await executeProcedure(procedureName, params);
+    return result; // Devuelve el resultado de la inserción
+  } catch (error) {
+    console.error('Error inserting payment account:', error);
+    throw error;
+  }
+};
+
+export const updatePaymentAccount = async (accountNumber, cardNumber, expirationDate, securityNumber, balance, status) => {
+  try {
+    const procedureName = 'sp_update_payment_account';
+    const params = {
+      AccountNumber: accountNumber,
+      CardNumber: cardNumber,
+      ExpirationDate: expirationDate,
+      SecurityNumber: securityNumber,
+      Balance: balance,
+      Status: status
+    };
+
+    const result = await executeProcedure(procedureName, params);
+    return result; // Devuelve el resultado de la actualización
+  } catch (error) {
+    console.error('Error updating payment account:', error);
+    throw error;
   }
 };
 
@@ -46,32 +143,36 @@ export const verifyAndUpdateUserInfo = async (
     status
   ) => {
     try {
-      if (oldPassword !== storedPassword) {
-        return { success: false, message: 'La contraseña antigua no coincide.' };
+      // Verifica si el usuario desea cambiar la contraseña
+      if (newPassword || confirmNewPassword) {
+        if (oldPassword !== storedPassword) {
+          return { success: false, message: 'The old password does not match.' };
+        }
+
+        if (newPassword !== confirmNewPassword) {
+          return { success: false, message: 'The new password does not match the confirmation.' };
+        }
       }
-  
-      if (newPassword !== confirmNewPassword) {
-        return { success: false, message: 'La nueva contraseña no coincide con la confirmación.' };
+
+      // Validaciones adicionales para otros campos
+      if (!firstName || !/^[A-Za-z\s]+$/.test(firstName)) {
+        return { success: false, message: 'First name cannot be empty and must contain only letters and spaces.' };
       }
-      
-      if (!firstName || !/^[A-Za-z]+$/.test(firstName)) {
-        return { success: false, message: 'El nombre no puede estar vacío y debe contener solo letras.' };
+
+      if (!secondName || !/^[A-Za-z\s]+$/.test(secondName)) {
+        return { success: false, message: 'Second name cannot be empty and must contain only letters and spaces.' };
       }
-  
-      if (!secondName || !/^[A-Za-z]+$/.test(secondName)) {
-        return { success: false, message: 'El segundo nombre no puede estar vacío y debe contener solo letras.' };
-      }
-  
+
       if (!email || !email.endsWith('@estudiantec.cr')) {
-        return { success: false, message: 'El email debe terminar en @estudiantec.cr.' };
+        return { success: false, message: 'The email must end with @estudiantec.cr.' };
       }
-  
+
       if (!phoneNumber || phoneNumber.length !== 8 || !/^\d+$/.test(phoneNumber)) {
-        return { success: false, message: 'El número de teléfono debe tener exactamente 8 dígitos.' };
+        return { success: false, message: 'The phone number must have exactly 8 digits.' };
       }
-  
+
       if (!workArea) {
-        return { success: false, message: 'El área de trabajo no puede estar vacía.' };
+        return { success: false, message: 'The work area cannot be empty.' };
       }
       
       // Ejecución del procedimiento de actualización si todas las verificaciones pasan
@@ -89,9 +190,9 @@ export const verifyAndUpdateUserInfo = async (
       };
   
       const result = await executeProcedure(procedureName, params);
-      return { success: true, message: 'Información actualizada correctamente.' };
+      return { success: true, message: 'Information updated successfully.' };
     } catch (error) {
-      console.error('Error al verificar y actualizar la información del usuario:', error);
-      return { success: false, message: 'Error al actualizar la información del usuario.' };
+      console.error('Error verifying and updating user information:', error);
+      return { success: false, message: 'Error updating user information.' };
     }
 };
--- rise-fund-android-version/views/USER_Config_View.js
+++ rise-fund-android-version/views/USER_Config_View.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, TextInput, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import styles from '../assets/Styles/Styles';
 import { AuthContext } from '../AuthContext';
-import { getUserInfo, verifyAndUpdateUserInfo } from '../controllers/USER_Config_Controller';
+import { getUserInfo, getAllRoles, getUserAccountInfo, generateUniqueAccountNumber, insertPaymentAccount, updatePaymentAccount, verifyAndUpdateUserInfo } from '../controllers/USER_Config_Controller';
 
 export default function USER_Config_View() {
   const { userID } = useContext(AuthContext);
@@ -23,6 +23,7 @@ export default function USER_Config_View() {
   const [newPassword, setNewPassword] = useState('');
   const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
+  const [hasAccount, setHasAccount] = useState(false);
   const [accountNumber, setAccountNumber] = useState('');
   const [cardNumber, setCardNumber] = useState('');
   const [expirationDate, setExpirationDate] = useState('');
@@ -35,23 +36,70 @@ export default function USER_Config_View() {
   // Cargar información del usuario cuando el componente se monta
   useEffect(() => {
     const fetchUserInfo = async () => {
-      const userInfo = await getUserInfo(userID);
-      if (userInfo) {
-        setUserId(userInfo.ID.toString());
-        setRoleId(userInfo.RoleId.toString());
-        setEmail(userInfo.Email);
-        setFirstName(userInfo.FirstName);
-        setSecondName(userInfo.SecondName);
-        setPhoneNumber(userInfo.PhoneNumber);
-        setWorkArea(userInfo.WorkArea);
-        setStatus(userInfo.Status);
-        setStoredPassword(userInfo.Password);
+      try {
+        // Obtener información del usuario
+        const userInfo = await getUserInfo(userID);
+        if (userInfo) {
+          setUserId(userInfo.ID.toString());
+          setEmail(userInfo.Email);
+          setFirstName(userInfo.FirstName);
+          setSecondName(userInfo.SecondName);
+          setPhoneNumber(userInfo.PhoneNumber);
+          setWorkArea(userInfo.WorkArea);
+          setStatus(userInfo.Status);
+          setStoredPassword(userInfo.Password);
+  
+          // Obtener todos los roles y encontrar el nombre del rol correspondiente
+          const roles = await getAllRoles();
+          const userRole = roles.find(role => role.ID === userInfo.RoleId);
+          console.log(userInfo.RoleId);
+          console.log(roles);
+          if (userRole) {
+            setRoleId(userRole.RoleName); // Establece el nombre del rol en lugar del ID
+          } else {
+            console.warn('Role not found for RoleId:', userInfo.RoleId);
+          }
+        }
+  
+        // Obtener información de la cuenta
+        const accountInfo = await getUserAccountInfo(userID);
+        if (accountInfo) {  // Si tiene cuenta
+          setAccountNumber(accountInfo.AccountNumber);
+          setCardNumber(accountInfo.CardNumber);
+          const formattedDate = accountInfo.ExpirationDate.substring(0, 10);
+          setExpirationDate(formattedDate);
+          setSecurityNumber(accountInfo.SecurityNumber.toString());
+          setAccountBalance(accountInfo.Balance.toString());
+          setBalanceAccountNumber(accountInfo.AccountNumber);
+          setHasAccount(true);
+        } else { // Si no tiene cuenta
+          const uniqueAccountNumber = await generateUniqueAccountNumber();
+          setAccountNumber(uniqueAccountNumber);
+          setHasAccount(false);
+        }
+      } catch (error) {
+        console.error('Error fetching user or role info:', error);
       }
     };
-
+  
     fetchUserInfo();
   }, [userID]);
 
+  const fetchAccountBalance = async () => {
+    try {
+      const accountInfo = await getUserAccountInfo(userID); // Usa el mismo método de consulta
+      if (accountInfo) {
+        setAccountBalance(accountInfo.Balance.toString()); // Actualiza solo el balance actual
+        setStatus(accountInfo.Status); // Actualiza el status actual
+      } else {
+        console.warn('No account found for the given user ID');
+      }
+    } catch (error) {
+      console.error('Error fetching account balance:', error);
+    }
+  };
+  
+  
   // Funciones onPress para cada botón
   const handleUpdateUserInfo = async  () => {
     const result = await verifyAndUpdateUserInfo(
@@ -71,14 +119,80 @@ export default function USER_Config_View() {
     Alert.alert('Update Info', result.message);
   };
 
-  const handleUpdatePaymentInfo = () => {
-    Alert.alert('Update Info', 'Payment info updated successfully!');
+  const handleUpdatePaymentInfo = async () => {
+    await fetchAccountBalance();
+    if (hasAccount) {
+      try {
+        // Llama a la función de actualización de cuenta
+        await updatePaymentAccount(
+          accountNumber,
+          cardNumber,
+          expirationDate,
+          securityNumber,
+          parseFloat(accountBalance), // Asegúrate de que el balance sea un número decimal
+          status
+        );
+        Alert.alert('Update Info', 'Payment info updated successfully!');
+        await fetchAccountBalance();
+      } catch (error) {
+        console.error('Error updating payment account:', error);
+        Alert.alert('Error', 'There was an error updating the account.');
+      }
+    } else {
+      try {
+        // Llama a la función de inserción de cuenta
+        await insertPaymentAccount(
+          accountNumber,
+          userId,
+          cardNumber,
+          expirationDate,
+          securityNumber,
+          parseFloat(0.1)
+        );
+        setHasAccount(true); // Cambia el estado a que ya tiene cuenta después de crearla
+        Alert.alert('Account Created', 'New payment account created successfully!');
+      } catch (error) {
+        console.error('Error creating payment account:', error);
+        Alert.alert('Error', 'There was an error creating the account.');
+      }
+    }
   };
 
-  const handleConfirmFunds = () => {
-    Alert.alert('Confirm', `Funds added: $${addAmount}`);
+  const handleConfirmFunds = async () => {
+    try {
+      // Verificar y obtener el balance actual y status
+      await fetchAccountBalance();
+      
+      // Convertir addAmount a número y sumar al balance actual
+      const amountToAdd = parseFloat(addAmount);
+      if (isNaN(amountToAdd) || amountToAdd <= 0) {
+        Alert.alert('Invalid Amount', 'Please enter a valid amount to add.');
+        return;
+      }
+  
+      const newBalance = parseFloat(accountBalance) + amountToAdd;
+  
+      // Actualizar el balance en la base de datos
+      await updatePaymentAccount(
+        accountNumber,
+        cardNumber,
+        expirationDate,
+        securityNumber,
+        newBalance, // Balance actualizado
+        status // Usa el status actual
+      );
+  
+      // Actualizar el estado en la interfaz para reflejar el nuevo balance
+      setAccountBalance(newBalance.toString());
+      setAddAmount(''); // Limpia el campo de entrada después de confirmar
+  
+      Alert.alert('Confirm', `Funds added successfully! New Balance: $${newBalance.toFixed(2)}`);
+    } catch (error) {
+      console.error('Error adding funds to account:', error);
+      Alert.alert('Error', 'There was an error adding funds to the account.');
+    }
   };
-
+  
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       {/* User Config Section */}
@@ -147,16 +261,86 @@ export default function USER_Config_View() {
         <Text style={styles.title}>Payment Config</Text>
 
         <Text style={styles.label}>Account Number</Text>
-        <TextInput style={styles.input} value={accountNumber} onChangeText={setAccountNumber} placeholder="Account Number" keyboardType="numeric" />
+        <TextInput 
+          style={styles.input} 
+          value={accountNumber} 
+          onChangeText={setAccountNumber} 
+          placeholder="Account Number" 
+          keyboardType="numeric" 
+          editable={false}
+        />
 
         <Text style={styles.label}>Card Number</Text>
-        <TextInput style={styles.input} value={cardNumber} onChangeText={setCardNumber} placeholder="Card Number" keyboardType="numeric" />
+        <TextInput 
+          style={styles.input} 
+          value={cardNumber} 
+          onChangeText={(text) => setCardNumber(text.slice(0, 16))} // Limita a 16 caracteres
+          placeholder="Card Number" 
+          keyboardType="numeric" 
+          editable={!hasAccount} 
+        />
 
         <Text style={styles.label}>Expiration Date</Text>
-        <TextInput style={styles.input} value={expirationDate} onChangeText={setExpirationDate} placeholder="mm/yy" />
+        <TextInput 
+          style={styles.input} 
+          value={expirationDate} 
+          onChangeText={(text) => {
+            // Remueve cualquier carácter no numérico
+            const cleanedText = text.replace(/[^0-9]/g, '');
+
+            let formattedText = cleanedText;
+
+            // Verificación del año, el mes y el día en sus respectivos rangos
+            if (cleanedText.length > 4) {
+              let year = parseInt(cleanedText.slice(0, 4), 10);
+
+              // Verifica que el año esté en el rango de 1950 a 2100
+              if (year < 1950) {
+                year = 1950;
+              } else if (year > 2100) {
+                year = 2100;
+              }
+              
+              const month = cleanedText.slice(4, 6);
+              formattedText = `${year}-${month}`;
+
+              // Verifica que el mes esté en el rango de 01 a 12
+              if (parseInt(month, 10) > 12) {
+                formattedText = `${year}-12`; // Si el mes es mayor a 12, lo establece en 12
+              } else if (month == '00') {
+                formattedText = `${year}-01`;
+              } else {
+                formattedText = `${year}-${month}`;
+              }
+            }
+
+            if (cleanedText.length > 6) {
+              const day = cleanedText.slice(6, 8);
+
+              // Verifica que el día esté en el rango de 01 a 31
+              if (parseInt(day, 10) > 31) {
+                formattedText = `${formattedText}-31`; // Si el día es mayor a 31, lo establece en 31
+              } else if(day == '00') {
+                formattedText = `${formattedText}-01`;
+              }else {
+                formattedText = `${formattedText}-${day}`;
+              }
+            }
+
+            setExpirationDate(formattedText); // Actualiza el estado con el texto formateado y validado
+          }} 
+          placeholder="yyyy-mm-dd" 
+          keyboardType="numeric" 
+        />
 
         <Text style={styles.label}>Security Number</Text>
-        <TextInput style={styles.input} value={securityNumber} onChangeText={setSecurityNumber} placeholder="Security Number" secureTextEntry keyboardType="numeric" />
+        <TextInput 
+          style={styles.input} 
+          value={securityNumber} 
+          onChangeText={(text) => setSecurityNumber(text.slice(0, 3))} // Limita a 3 caracteres
+          placeholder="Security Number" 
+          keyboardType="numeric" 
+        />
 
         <TouchableOpacity style={styles.button} onPress={handleUpdatePaymentInfo}>
           <Text style={styles.buttonText}>Update Info</Text>
@@ -168,7 +352,7 @@ export default function USER_Config_View() {
         <Text style={styles.title}>Monetary Account Config</Text>
 
         <Text style={styles.label}>Account Number</Text>
-        <TextInput style={styles.input} value={balanceAccountNumber} onChangeText={setBalanceAccountNumber} placeholder="Account Number" keyboardType="numeric" />
+        <TextInput style={styles.input} value={balanceAccountNumber} onChangeText={setBalanceAccountNumber} placeholder="Account Number" keyboardType="numeric" editable={false} />
 
         <Text style={styles.label}>Account Balance</Text>
         <TextInput style={styles.input} value={accountBalance} onChangeText={setAccountBalance} placeholder="Account balance" editable={false} />
