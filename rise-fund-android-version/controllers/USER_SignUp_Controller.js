90076e3b311cc8051290e7345125c0b0d6d5b5b0
c4dd3 <danielowu1012@gmail.com>
2024-11-04 18:48:15 -0600

    Registers login y signup

--- rise-fund-android-version/controllers/USER_SignUp_Controller.js
+++ rise-fund-android-version/controllers/USER_SignUp_Controller.js
@@ -1,4 +1,12 @@
 import { executeProcedure } from '../database/apiService';
+import {insertRegister} from '../controllers/SYSTEM_Register_Controller'
+const handleInsertRegister = async (type, detail) => {
+  try {
+    await insertRegister(type, detail);
+  } catch (error) {
+    console.error('Error inserting register:', error);
+  }
+};
 
 export const addUser = async ({ ID, RoleId, Email, FirstName, SecondName, PhoneNumber, WorkArea, Password, Status }) => {
   try {
@@ -18,6 +26,7 @@ export const addUser = async ({ ID, RoleId, Email, FirstName, SecondName, PhoneN
     const result = await executeProcedure(procedureName, params);
     if (result[0].length !== 0) {
       console.log('New user added with ID:', result[0][0].NewUserID);
+      await handleInsertRegister(1, `User ${result[0][0].NewUserID} created their account!`); // 1 Usuario - 2 Donacion - 3 Proyecto
       return(result);
     } else {
       console.log('User creation failed.');
